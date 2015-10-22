7dd8c15b5b3dcce86d222d965dafa714f0d9be66
Watson <watson0x90@gmail.com>
2015-10-22 16:49:54 -0600

    Update Implant-Cred.ps1

--- Implant-Cred.ps1
+++ Implant-Cred.ps1
@@ -8,7 +8,7 @@
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Find-Files.ps1 is distributed in the hope that it will be useful,
+# Implant-Cred.ps1 is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
